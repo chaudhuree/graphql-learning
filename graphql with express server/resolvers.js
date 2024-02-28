02532c9064a7cc39585c6294573af8c8d4ae83e5
chaudhuree <chaudhuree00@gmail.com>
2024-02-28 09:27:25 +0600

    nullable and array type schema

--- graphql with express server/resolvers.js
+++ graphql with express server/resolvers.js
@@ -1,5 +1,19 @@
 export const resolvers = {
   Query: {
-    greeting: () => 'hello chaudhuree'
-  }
+    greeting: () => "hello chaudhuree",
+    jobs: () => {
+      return [
+        {
+          id: "1",
+          title: "Software Engineer",
+          description: "Some description about the job",
+        },
+        {
+          id: "2",
+          title: "Backend Engineer",
+          description: "Some description about the job",
+        },
+      ];
+    },
+  },
 };
