24180561deb841d9aecef76038a105710e35283c
chaudhuree <chaudhuree00@gmail.com>
2024-02-28 10:49:43 +0600

    getting jobs done from db

--- graphql with express server/resolvers.js
+++ graphql with express server/resolvers.js
@@ -1,19 +1,23 @@
+import { getJobs } from "./db/jobs.js";
 export const resolvers = {
   Query: {
     greeting: () => "hello chaudhuree",
-    jobs: () => {
-      return [
-        {
-          id: "1",
-          title: "Software Engineer",
-          description: "Some description about the job",
-        },
-        {
-          id: "2",
-          title: "Backend Engineer",
-          description: "Some description about the job",
-        },
-      ];
+    jobs: async () => {
+      const jobs = await getJobs();
+      console.log(jobs);
+      return jobs;
+      // return [
+      //   {
+      //     id: "1",
+      //     title: "Software Engineer",
+      //     description: "Some description about the job",
+      //   },
+      //   {
+      //     id: "2",
+      //     title: "Backend Engineer",
+      //     description: "Some description about the job",
+      //   },
+      // ];
     },
   },
 };
