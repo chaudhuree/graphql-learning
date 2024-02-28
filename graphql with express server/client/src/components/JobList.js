6b3bba8cd9f4218373735313e85b3dc3c0447f0e
chaudhuree <chaudhuree00@gmail.com>
2024-02-28 14:51:24 +0600

    show in the ui the fetched data

--- graphql with express server/client/src/components/JobList.js
+++ graphql with express server/client/src/components/JobList.js
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
 import { formatDate } from '../lib/formatters';
+import { useEffect, useState } from 'react';
 import { getJobs } from '../lib/graphql/queries';
 
-getJobs().then((data) => {
-  console.log(data);
-});
+// getJobs().then((data) => {
+//   console.log(data);
+// });
 
-function JobList({ jobs }) {
+function JobList() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then((data) => {
+      setJobs(data);
+    });
+  }, []);
+
   return (
     <ul className="box">
       {jobs.map((job) => (
