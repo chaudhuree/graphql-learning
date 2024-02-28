0b3f4d608975d60e4e7c681ad02757faf8629312
chaudhuree <chaudhuree00@gmail.com>
2024-02-28 11:25:13 +0600

    refactor jobs, get date from db as createdAt format it and serve it to the query as date

--- graphql with express server/resolvers.js
+++ graphql with express server/resolvers.js
@@ -1,23 +1,15 @@
 import { getJobs } from "./db/jobs.js";
 export const resolvers = {
   Query: {
-    greeting: () => "hello chaudhuree",
     jobs: async () => {
-      const jobs = await getJobs();
-      console.log(jobs);
-      return jobs;
-      // return [
-      //   {
-      //     id: "1",
-      //     title: "Software Engineer",
-      //     description: "Some description about the job",
-      //   },
-      //   {
-      //     id: "2",
-      //     title: "Backend Engineer",
-      //     description: "Some description about the job",
-      //   },
-      // ];
+      return await getJobs();
     },
   },
+  Job: {
+    date: (job) => formatedData(job.createdAt),
+  },
 };
+
+function formatedData(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
